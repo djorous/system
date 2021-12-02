b9903573fce3c005e2fcdc0d3542efe5159770fc
Douglas Osterkamp <86150785+djorous@users.noreply.github.com>
2021-12-02 13:43:44 +0000

    Update Partitioning.sh

--- Partitioning.sh
+++ Partitioning.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +100M # 100 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
-EOF
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' | fdisk /dev/sda
+  g     # clear the in memory partition table
+  n     # new partition
+  1     # partition number 1
+        # default - start at beginning of disk 
+  +512M # 100 MB boot parttion
+  n     # new partition
+  2     # partion number 2
+        # default, start immediately after preceding partition
+  +4G   # default, extend partition to end of disk
+  n     # new partition
+  3     # partion number 2
+        # default, start immediately after preceding partition
+  +25G  # default, extend partition to end of disk
+  n     # new partition
+  4     # partion number 2
+        # default, start immediately after preceding partition
+        # default, extend partition to end of disk
+  t     # change type
+  1     # partition number 1
+  uefi  # partition type
+  t     # change type
+  2     # partition number 1
+  swap  # partition type
+  p     # print the in-memory partition table
+  w     # write the partition table
