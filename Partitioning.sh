a5134fd2e0183b4498effbc90be514359e6ef779
Douglas Osterkamp <86150785+djorous@users.noreply.github.com>
2021-12-02 14:34:01 +0000

    Update Partitioning.sh

--- Partitioning.sh
+++ Partitioning.sh
@@ -24,4 +24,5 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   2 # partion number 2
   swap  # default, start immediately after preceding partition  
   p # print the in-memory partition table
+  w # write
 EOF
