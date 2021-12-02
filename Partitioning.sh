6c94304456033ed8d596dceb4ae5e37b0bcb19bc
Douglas Osterkamp <86150785+djorous@users.noreply.github.com>
2021-12-02 14:13:33 +0000

    Update Partitioning.sh

--- Partitioning.sh
+++ Partitioning.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' | fdisk << EOF | fdisk /dev/sda
-g
-n
-1
- 
-+512M
-#n
-#2
- 
-#+4G
-#n
-#3
-# 
-#+25G
-#n
-#4
-# 
-# 
-#t
-#1
-#uefi
-#t
-#2
-#swap
-#p
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  g # clear the in memory partition table
+  n # new partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +512M # 100 MB boot parttion
+  n # new partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  p # print the in-memory partition table
 EOF
