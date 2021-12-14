f2e2114f5679d32b72dba3bb9012a28d02f0c485
djorous <djorous@gmail.com>
2021-12-14 19:48:15 +0000

    a

--- setup_test.sh
+++ setup_test.sh
@@ -105,7 +105,8 @@ mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@ /dev/"${dis
 btrfs quota enable /mnt
 
 #Create mount point directories
-mkdir /mnt/{boot,home,.snapshots,var/log}
+mkdir /mnt/{boot,home,.snapshots,var}
+mkdir /mnt/var/log
 
 #Mount /home subvolume
 mount -o noatime,compress=zstd,space_cache=v2,discard=async,subvol=@home /dev/"${diskname}3" /mnt/home
