e02b077fd161d2c91beb5153f200991052c2286e
Douglas Osterkamp <86150785+djorous@users.noreply.github.com>
2024-05-20 15:55:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,7 +61,6 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/$diskname
   t # new partition
   1 # partion number 1
   uefi  # default, start immediately after preceding partition
-  t # new partition
   p # print the in-memory partition table
   w # write
 EOF
